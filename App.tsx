2a57813ec071c43b39d22fad39badb5fb3a75c41
Myles Fang <git@myles.hk>
2024-01-16 22:20:17 +0800

    Minimal reproducible

--- App.tsx
+++ App.tsx
@@ -1,117 +1,127 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
+  Album,
+  CameraRoll,
+  PhotoIdentifier,
+} from '@react-native-camera-roll/camera-roll';
+import React, {useCallback, useEffect, useState} from 'react';
+import {
+  Button,
+  FlatList,
+  Image,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): JSX.Element {
+  const [albums, setAlbums] = useState<Album[]>([]);
+  const [photos, setPhotos] = useState<PhotoIdentifier[]>([]);
+  const [selectedAlbum, setSelectedAlbum] = useState<Album | null>(null);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const loadAlbums = useCallback(async () => {
+    const _albums = await CameraRoll.getAlbums({
+      assetType: 'All',
+      albumType: 'All',
+    });
+    setAlbums(_albums);
+  }, [setAlbums]);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+  const selectAlbum = useCallback(
+    async (album: Album) => {
+      const _photos = (
+        await CameraRoll.getPhotos({
+          first: 10,
+          groupTypes: album.type,
+          groupName: album.title,
+          include: [
+            'fileSize',
+            'filename',
+            'albums',
+            'fileExtension',
+            'imageSize',
+            'location',
+            'orientation',
+            'playableDuration',
+          ],
+        })
+      ).edges;
+
+      setSelectedAlbum(album);
+      setPhotos(_photos);
+    },
+    [setPhotos, setSelectedAlbum],
   );
-}
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  useEffect(() => {
+    loadAlbums().then(() => {
+      console.log('Loaded');
+    });
+  }, [loadAlbums]);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.area}>
+      {selectedAlbum && (
+        <Button
+          title="Back"
+          onPress={() => {
+            setSelectedAlbum(null);
+            setPhotos([]);
+          }}
+        />
+      )}
+      {selectedAlbum ? (
+        <FlatList
+          horizontal
+          contentContainerStyle={styles.listContent}
+          data={photos}
+          renderItem={({item: photo}) => (
+            <View>
+              <Text style={styles.item}>
+                {JSON.stringify(photo.node, null, 2)}
+              </Text>
+              <Image
+                source={{uri: photo.node.image.uri}}
+                style={styles.image}
+              />
+            </View>
+          )}
+          keyExtractor={photo => photo.node.id}
+        />
+      ) : (
+        <FlatList
+          contentContainerStyle={styles.listContent}
+          data={albums}
+          renderItem={({item: album}) => (
+            <TouchableOpacity
+              onPress={() => {
+                selectAlbum(album);
+              }}>
+              <Text style={styles.item}>{JSON.stringify(album, null, 2)}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={album => album.title}
+        />
+      )}
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  area: {
+    flex: 1,
+    backgroundColor: 'white',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  listContent: {
+    padding: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  item: {
+    marginHorizontal: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  image: {
+    height: 300,
+    marginHorizontal: 10,
   },
 });
 
